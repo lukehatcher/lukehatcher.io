699e2022052cde55606830fca7c1c82569757863
Luke Hatcher <lukehatcher98@gmail.com>
2021-09-20 23:10:34 -0700

    make header and footer absolute

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,18 +7,31 @@ import { SiGithub } from '@react-icons/all-files/si/SiGithub';
 import { SiGmail } from '@react-icons/all-files/si/SiGmail';
 import { FaFileAlt } from '@react-icons/all-files/fa/FaFileAlt';
 
+enum directions {
+  ROW = 'row',
+  COLUMN = 'column',
+}
+
+interface IconContainerProps {
+  flexDirection: string;
+}
+
 const PageContainer = styled.div`
+  min-height: 100vh; // ========
+  position: relative; // ========
   display: flex;
   flex-direction: column;
   box-sizing: border-box;
   font-family: SF Pro Text, SF Pro Icons, Helvetica Neue, Helvetica, Arial, sans-serif;
   color: #f5f6f7;
-  justify-content: center;
+  /* justify-content: center; */
   align-items: center;
 `;
 
 const HeaderContainer = styled.div`
-  height: 10vh;
+  position: absolute; // ========
+  top: 0;
+  height: 10vh; // ========
   width: 100vw;
   display: flex;
   background-color: rebeccapurple;
@@ -27,28 +40,34 @@ const HeaderContainer = styled.div`
   text-align: center;
 `;
 
+const ContentContainer = styled.div`
+  margin-top: 15vh;
+  padding-bottom: 80px; // footer height
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  /* border: 1px solid green; */
+`;
+
 const AboutBlurbContainer = styled.div`
-  margin-top: 75px;
   margin-bottom: 75px;
   max-width: 680px;
-  /* border: 1px solid red; */
   padding: 30px;
+  /* border: 1px solid red; */
 `;
 
 const BlurbContent = styled.p`
   font-size: 16px;
   line-height: 1.65;
+  padding-left: 15px;
+  padding-right: 15px;
 `;
-
-interface IconContainerProps {
-  flexDirection: string;
-}
 
 const RootIconContainer = styled.div<IconContainerProps>`
   display: flex;
   flex-direction: ${(props) => props.flexDirection};
   padding: 15px;
-  /* margin-top: 150px; */
 `;
 
 const IconContainer1 = styled.div<IconContainerProps>`
@@ -67,23 +86,27 @@ const IconContainer2 = styled.div<IconContainerProps>`
 
 const FooterContainer = styled.div`
   position: absolute;
-  bottom: 0px;
+  display: flex;
+  flex-direction: column;
+  bottom: 0;
   width: 100vw;
+  height: 80px; // footer height
   background-color: #1c1e21;
   text-align: center;
-  padding: 30px;
+  align-items: center;
+  justify-content: center;
 `;
 
-enum directions {
-  ROW = 'row',
-  COLUMN = 'column',
-}
+const FooterText = styled.p`
+  font-size: 14px;
+  margin-top: 5px;
+`;
 
 const Index = () => {
   const [rootFlexDirection, setRootFlexDirection] = useState<directions>(directions.ROW);
   const [flexDirection, setFlexDirection] = useState<directions>(directions.ROW);
 
-  const throttle = (fxn: (...args: any[]) => void, timeout: number = 300) => {
+  const throttle = (fxn: (...args: any[]) => void, timeout: number = 200) => {
     let wait = false;
     return (...args: any) => {
       if (!wait) {
@@ -98,6 +121,7 @@ const Index = () => {
 
   const handleResize = () => {
     if (window.innerWidth < 960) {
+      // 960, 680
       setRootFlexDirection(directions.COLUMN);
     } else {
       setRootFlexDirection(directions.ROW);
@@ -117,44 +141,50 @@ const Index = () => {
       <HeaderContainer>
         <h1>Luke Hatcher</h1>
       </HeaderContainer>
-      <AboutBlurbContainer>
-        <p>Hi,</p>
-        <br />
-        <BlurbContent>
-          &nbsp;&nbsp;&nbsp;&nbsp; I'm Luke, a software engineer. I graduated from the University of Washington in 2020
-          with a B.S. in chemistry and a minor in math. I fell in love with programming while working in a computational
-          chemistry research group at UW. I am passionate about building products that simplify peoples lives. In my
-          free time I enjoy working on thoughtBubble, a productivity ecosystem for developers where they can sync their
-          thoughts between their mobile devices and their code editors. Check it out below! My current programming
-          interests revolve around TypeScript, C# and the occasional Python. Feel free to get in touch!
-        </BlurbContent>
-      </AboutBlurbContainer>
-      <RootIconContainer flexDirection={rootFlexDirection}>
-        <IconContainer1 flexDirection={flexDirection}>
-          <a href="https://github.com/lukehatcher" rel="noreferrer" target="_blank" title="GitHub Profile">
-            <SiGithub size="85" />
-          </a>
-          <a href="https://www.linkedin.com/in/lukehatcher98/" rel="noreferrer" target="_blank" title="LinkIn Profile">
-            <SiLinkedin size="85" />
-          </a>
-        </IconContainer1>
-        <IconContainer2 flexDirection={flexDirection}>
-          <a href="mailto:lukehatcher98@gmail.com" rel="noreferrer" target="_blank" title="Gmail">
-            <SiGmail size="85" />
-          </a>
-          <a
-            href="https://luke-resume.s3-us-west-2.amazonaws.com/lukeHatcher-resume.pdf"
-            target="_blank"
-            title="Resume"
-          >
-            <FaFileAlt size="85" />
-          </a>
-        </IconContainer2>
-      </RootIconContainer>
+      <ContentContainer>
+        <AboutBlurbContainer>
+          <p>Hi,</p>
+          <br />
+          <BlurbContent>
+            &nbsp;&nbsp;&nbsp;&nbsp; I'm Luke, a software engineer. I graduated from the University of Washington in
+            2020 with a B.S. in chemistry and a minor in math. I fell in love with programming while working in a
+            computational chemistry research group at UW. I am passionate about building products that simplify peoples
+            lives. In my free time I enjoy working on thoughtBubble, a productivity ecosystem for developers where they
+            can sync their thoughts between their mobile devices and their code editors. Check it out below! My current
+            programming interests revolve around TypeScript, C# and the occasional Python. Feel free to get in touch!
+          </BlurbContent>
+        </AboutBlurbContainer>
+        <RootIconContainer flexDirection={rootFlexDirection}>
+          <IconContainer1 flexDirection={flexDirection}>
+            <a href="https://github.com/lukehatcher" rel="noreferrer" target="_blank" title="GitHub Profile">
+              <SiGithub size="85" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/lukehatcher98/"
+              rel="noreferrer"
+              target="_blank"
+              title="LinkIn Profile"
+            >
+              <SiLinkedin size="85" />
+            </a>
+          </IconContainer1>
+          <IconContainer2 flexDirection={flexDirection}>
+            <a href="mailto:lukehatcher98@gmail.com" rel="noreferrer" target="_blank" title="Gmail">
+              <SiGmail size="85" />
+            </a>
+            <a
+              href="https://luke-resume.s3-us-west-2.amazonaws.com/lukeHatcher-resume.pdf"
+              target="_blank"
+              title="Resume"
+            >
+              <FaFileAlt size="85" />
+            </a>
+          </IconContainer2>
+        </RootIconContainer>
+      </ContentContainer>
       <FooterContainer>
-        <p>© Copyright 2021, Luke Hatcher</p>
-        <br />
-        <p>📍 Seattle, WA</p>
+        <FooterText>© Copyright 2021, Luke Hatcher</FooterText>
+        <FooterText>📍 Seattle, WA</FooterText>
       </FooterContainer>
     </PageContainer>
   );
