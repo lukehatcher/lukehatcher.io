e3673b31325055791e41ef4c44ba3e8b645ea252
Luke Hatcher <lukehatcher98@gmail.com>
2021-09-21 02:49:52 -0700

    vw

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -72,7 +72,7 @@ const BlurbContent = styled.p`
   /* font-size: calc(0.5vw + 0.5vh + 0.6vmin); // first working one */
   /* font-size: calc(13px + 0.7vmin); // latest */
   /* font-size: calc(12px + 0.8vmin); */
-  font-size: calc(13px + 0.5vmin);
+  font-size: calc(13px + 0.5vmax);
 
   line-height: 1.65;
   padding-left: 15px;
